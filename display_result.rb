276f223f739d8054ff7dc7f10d30142eccd55fc7
Koki Hashimoto <gohannnotomo@gmail.com>
2025-05-18 12:43:31 +0900

    血圧データ修正
    - 起床時は一番時刻の古いものを使うようにした
    - 計測時の心拍数も出すようにした

--- display_result.rb
+++ display_result.rb
@@ -119,9 +119,9 @@ bp_y = bp_y.max_by { |e| e['measurementTimestampGMT'] }
 bp_y['time'] = to_jst(bp_y['measurementTimestampGMT'])
 bp_t = data['bp_raw'] || []
 bp_t = bp_t[0]['measurements']
-bp_t = bp_t.max_by { |e| e['measurementTimestampGMT'] }
+bp_t = bp_t.min_by { |e| e['measurementTimestampGMT'] }
 bp_t['time'] = to_jst(bp_t['measurementTimestampGMT'])
 
 puts "■ 血圧"
-puts "- 就寝前: #{bp_y['systolic']} - #{bp_y['diastolic']} 時刻=#{bp_y['time'].strftime('%H:%M')}"
-puts "- 起床時: #{bp_t['systolic']} - #{bp_t['diastolic']} 時刻=#{bp_t['time'].strftime('%H:%M')}"
+puts "- 就寝前: #{bp_y['systolic']}/#{bp_y['diastolic']} #{bp_y['pulse']}bpm 時刻=#{bp_y['time'].strftime('%H:%M')}"
+puts "- 起床時: #{bp_t['systolic']}/#{bp_t['diastolic']} #{bp_t['pulse']}bpm 時刻=#{bp_t['time'].strftime('%H:%M')}"
